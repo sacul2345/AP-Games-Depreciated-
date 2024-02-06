3cc778653af4f480f09a5c1b62edeeb711e15d14
Lucas <49418303+sacul2345@users.noreply.github.com>
2024-02-05 20:47:42 -0600

    Delete shapez.io-master directory

--- shapez.io-master/electron/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("ipcRenderer", {
-    invoke: ipcRenderer.invoke.bind(ipcRenderer),
-    on: ipcRenderer.on.bind(ipcRenderer),
-    send: ipcRenderer.send.bind(ipcRenderer),
-});
--- shapez.io-master/electron_wegame/README.md
+++
@@ -1,1 +0,0 @@
-To build, place the lib64 folder from the wegame sdk for electron 13 in `wegame_sdk` and run the `wegame.main.standalone` gulp task.
